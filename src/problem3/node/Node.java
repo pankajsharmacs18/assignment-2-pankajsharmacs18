c819406038791c9c43a9da6c6a9db75eea6cac05
pankaj sharma <47051537+pankajsharmacs18@users.noreply.github.com>
2020-03-27 13:31:08 +0530

    Update Node.java

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -1,10 +1,39 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:13 PM
- */
+
+
 package problem3.node;
 
+
+import problem5.student.Student;
+
 public class Node {
+
+
+    private Student s;
+    private  Node next;
+
+
+    public Student getS() {
+        return s;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public Node() {
+        s=new Student();
+        next=null;
+    }
+
+    public void setS(Student s) {
+        this.s = s;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+
+
+
 }
