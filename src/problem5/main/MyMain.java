9f1df896a7eb4bf68577a3548520a972cf42a3d8
pankaj sharma <47051537+pankajsharmacs18@users.noreply.github.com>
2020-03-27 13:35:53 +0530

    Update MyMain.java

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -2,9 +2,46 @@
  *  Created by IntelliJ IDEA.
  *  User: Vaibhav
  *  Date: 23-Mar-20
- *  Time: 7:06 PM
+ *  Time: 7:06 package problem5.main;
+
+import problem5.circularqueue.MyCircularQueue;
+import problem5.node.Node;
+import problem5.student.Student;
+
+import java.util.Scanner;
+
+//executable class
+public class MyMain
+{
+    public static void main(String... PankajSharma)
+    {
+        MyCircularQueue m=new MyCircularQueue();
+        Node node;
+
+        for (int i = 0; i <5 ; i++)
+        {
+            node=new Node(new Student());
+            m.enqueue(node);
+        }
+        m.printQueue();
+
+        m.remove(new Scanner(System.in).next());
+        m.printQueue();
+
+        m.process(new Scanner(System.in).next());
+
+
+
+
+
+
+
+
+    }
+}
+
  */
 package problem5.main;
-//executable class
+//executable class;;
 public class MyMain {
 }
